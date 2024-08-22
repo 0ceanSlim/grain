7430b674f01ba2a04cfa568987fc8d5207240095
0ceanSlim <chriskerr@gmx.com>
2024-08-22 13:03:33 -0400

    added bson to event type

--- server/types/event.go
+++ server/types/event.go
@@ -1,11 +1,11 @@
 package relay
 
 type Event struct {
-	ID        string     `json:"id"`
-	PubKey    string     `json:"pubkey"`
-	CreatedAt int64      `json:"created_at"`
-	Kind      int        `json:"kind"`
-	Tags      [][]string `json:"tags"`
-	Content   string     `json:"content"`
-	Sig       string     `json:"sig"`
+	ID        string     `json:"id" bson:"id"`
+	PubKey    string     `json:"pubkey" bson:"pubkey"`
+	CreatedAt int64      `json:"created_at" bson:"created_at"`
+	Kind      int        `json:"kind" bson:"kind"`
+	Tags      [][]string `json:"tags" bson:"tags"`
+	Content   string     `json:"content" bson:"content"`
+	Sig       string     `json:"sig" bson:"sig"`
 }
