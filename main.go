36e9881c3d32e1e5d13c1e369d6af5aaa0a0cd0c
Chris kerr <chriskerr@gmx.com>
2025-05-31 09:51:46 -0400

    remove commented line in main

--- main.go
+++ main.go
@@ -96,7 +96,6 @@ func main() {
 		config.InitializePubkeyCache()
 
 		// Start event purging in the background.
-		//go mongo.ScheduleEventPurging(cfg)
 		go mongo.ScheduleEventPurgingOptimized(cfg)
 
 		// Start periodic user sync in a goroutine
